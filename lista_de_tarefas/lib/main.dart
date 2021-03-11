866e8b5b20d6a9438c95f7ba1b59ce6d033f5a65
ivanilsonsoares <ivanilson.soares.is@gmail.com>
2021-03-10 21:48:32 -0300

    SnaclBar e Desfazendo ação

--- lista_de_tarefas/lib/main.dart
+++ lista_de_tarefas/lib/main.dart
@@ -20,6 +20,8 @@ class _HomeState extends State<Home> {
 
   final _toDoController = TextEditingController();
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
 
   @override
@@ -87,7 +89,7 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Widget buildItem(context, index) {
+  Widget buildItem(BuildContext context,int index) {
     return Dismissible(
       key: Key(DateTime.now().microsecondsSinceEpoch.toString()),
       background: Container(
@@ -113,6 +115,28 @@ class _HomeState extends State<Home> {
           });
         },
       ),
+      onDismissed: (direction){
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content : Text("Tarefa \"${_lastRemoved["title"]}\" removido!"),
+            action: SnackBarAction(label: "Desfazer",
+            onPressed: (){
+              setState(() {
+                _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                _saveData(); 
+              });
+            }),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
